c7ea5fc68b732582f751d4b7646fdf5b9ad88edb
Daniel Siladji <szilagyi.d@gmail.com>
2017-05-13 15:02:05 +0200

    Updated helper scripts

--- src/util/plot_score_matrix.py
+++ src/util/plot_score_matrix.py
@@ -2,24 +2,57 @@ from matrix_loader import load
 from matplotlib import pyplot as plt
 from pathlib import Path
 from subprocess import run, PIPE
+import math
 
 import sys
 
-path = Path(sys.argv[1] if len(sys.argv) >
-            1 else r'..\..\data\dipeptide-at-least-four.orthoset.fasta')
 
-bin_path = path.with_suffix('.bin')
-fasta_path = bin_path.with_suffix('.fasta')
+def plot_single(path, ax):
+    if not path.exists():
+        print("Path", path, "does not exist")
+        sys.exit(1)
 
-if not bin_path.exists():
-    run([r'..\..\build-win\fastscore', str(fasta_path)])
+    bin_path = path.with_suffix('.bin')
+    fasta_path = bin_path.with_suffix('.fasta')
 
-mm = load(str(bin_path))
+    if not bin_path.exists():
+        run([r'..\..\build-win\fastscore', str(fasta_path)])
 
-fig, ax1 = plt.subplots()
-cax = ax1.imshow(mm, cmap='hot')
-fig.colorbar(cax)
+    mm = load(str(bin_path))
+    im = ax.imshow(mm, cmap='hot')
+    return im
 
-# ax2.hist(mm.ravel())
+
+if len(sys.argv) < 2:
+    sys.argv.append(r"""..\..\data\tetraheptads.orthoset.bin""")
+
+paths = [Path(p) for p in sys.argv[1:]]
+
+n = len(paths)
+if n <= 3:
+    dimensions = [(1, 1), (1, 2), (2, 2)]
+    (nrows, ncols) = dimensions[n - 1]
+else:
+    divisors = [num for num in range(
+        1, round(math.sqrt(n)) + 1) if n % num == 0 and num * num <= n]
+    nrows = divisors[-1]
+    ncols = n // nrows
+
+fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
+
+if n == 1:
+    axes.flat = [axes]
+    display_axes = True
+else:
+    display_axes = False
+
+for path, ax in zip(paths, axes.flat):
+    im = plot_single(path, ax)
+
+if not display_axes:
+    for ax in axes.flat:
+        ax.axis('off')
+
+fig.colorbar(im, ax=list(axes.flat))
 
 plt.show()
--- src/util/recommend_cutoff.py
+++ src/util/recommend_cutoff.py
@@ -1,16 +1,24 @@
 import os
 import math
+import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, rv_histogram
 from matrix_loader import load
+from pathlib import Path
 
 weak_percentile = 0.21450289100201259
 strong_percentile = 0.0033595580728521786
 
-path = r"""..\..\data\dipeptide-at-least-four.bin"""
-data = load(path)
+path = Path(sys.argv[1] if len(sys.argv) >
+            1 else r"""..\..\data\dipeptide-at-least-four.bin""")
+
+if not path.exists():
+    print("Path", path, "does not exist")
+    sys.exit(1)
+
+data = load(str(path))
 
 
 def hist(data, nbins=100):
@@ -21,15 +29,25 @@ def hist(data, nbins=100):
     plt.xlim(minmax)
 
     mu, std = norm.fit(data)
+    real_dist = rv_histogram((h, bins))
 
     x = (bins[1:] + bins[:-1]) / 2
     gauss = norm.pdf(x, mu, std)
-    plt.plot(x, gauss, 'r')
+    plt.plot(x, gauss, 'k.')
+
+    weak_cutoff = real_dist.ppf(weak_percentile)
+    strong_cutoff = real_dist.ppf(strong_percentile)
+
+    weak_cutoff_gaussian = norm.ppf(weak_percentile, mu, std)
+    strong_cutoff_gaussian = norm.ppf(strong_percentile, mu, std)
+
+    plt.axvline(x=weak_cutoff, color='r')
+    plt.axvline(x=strong_cutoff, color='r')
+
+    plt.axvline(x=weak_cutoff_gaussian, color='k')
+    plt.axvline(x=strong_cutoff_gaussian, color='k')
 
     plt.show()
-
-    weak_cutoff = norm.ppf(weak_percentile, mu, std)
-    strong_cutoff = norm.ppf(strong_percentile, mu, std)
 
     return weak_cutoff, strong_cutoff
 
